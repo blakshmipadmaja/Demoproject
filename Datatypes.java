e67440039b41d053faafcd6793956f70f9e219b9
blakshmipadmaja <141156207+blakshmipadmaja@users.noreply.github.com>
2023-11-03 19:06:58 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,31 @@
+public class Datatypes {
+    public static void main(String a[])
+    {
+        int num1 = 9;
+        byte by = 127;
+        short sh= 5586;
+        long l = 585466552300654l;
+
+        float f = 593445622f;
+        double d = 58654338;
+
+        char c = 'k';
+
+        boolean b = false; 
+         boolean b1 = true; 
+        System.out.println(num1);
+        System.out.println(by);
+        System.out.println(sh);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+         System.out.println(b);
+         System.out.println(b1);
+    }
+    
+   
+
+
+    
+}
